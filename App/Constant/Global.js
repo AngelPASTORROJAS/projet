c2a17ccf874e9b46553f31a2002fb6bacef4b3a9
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-04-28 11:06:55 +0200

    fix: correction des la creation de la database si elle n'existe pas

--- App/Constant/Global.js
+++ App/Constant/Global.js
@@ -1,11 +1,9 @@
-
 require("dotenv-expand").expand(require("dotenv").config());
-const mysql2 = require('mysql2');
+const mysql2 = require("mysql2");
 
 const APP = Object.freeze({
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
 });
-
 
 const connection = mysql2.createConnection({
   host: process.env.DB_HOST,
@@ -14,14 +12,22 @@ const connection = mysql2.createConnection({
   multipleStatements: true,
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
+connection.connect(function (err) {
+  if (err) {
+    console.error("Failed to connect to MySQL server:", err);
+    return;
+  }
   console.log("Connected to MySQL server!");
-  
-  connection.query(`CREATE DATABASE ${process.env.DB_NAME}`, function (err, result) {
-    if (err) throw err;
-    console.log("Database created!");
-  });
+  connection.query(
+    `CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`,
+    function (err, result) {
+      if (err) {
+        console.error("Failed to create database:", err);
+        return;
+      }
+      console.log("Database created!");
+    }
+  );
 });
 
-module.exports = {connection, APP};
+module.exports = { connection, APP };
